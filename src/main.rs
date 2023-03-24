cb23925dfc55b35e3187ca55ec1a9a6f84024b08
semi <semilin@disroot.org>
2023-03-23 20:31:52 -0700

    add reading data, search

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use iced::widget::{button, container, text, text_input, column, row, scrollable, vertical_rule};
+use iced::widget::{button, container, text, text_input, column, row, scrollable};
 use iced::{Element, Sandbox, Settings, Point, Theme, Length, Rectangle};
-use iced::{keyboard, mouse};
+//use iced::{keyboard, mouse};
 use serde::{Serialize, Deserialize};
 use ron::ser::{to_string_pretty, PrettyConfig};
 use directories::ProjectDirs;
 use indexmap::IndexMap;
 use std::fs::{self, File};
 use std::path::Path;
-use std::io::Write;
+use std::io::{Read, Write};
+use fuzzy_matcher::FuzzyMatcher;
+use fuzzy_matcher::skim::SkimMatcherV2;
+
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 enum NodeVal {
@@ -85,44 +88,69 @@ pub fn main() -> iced::Result {
 
 struct Aweb {
     graph: Graph,
+    filtered_nodes: Vec<String>,
+    filtered_edges: Vec<[String; 3]>,
     node_input: String,
+    search_input: String,
 }
 
 impl Aweb {
+    fn filter(&mut self) {
+	let matcher = SkimMatcherV2::default();
+	self.filtered_nodes = self.graph.nodes.keys().flat_map(|s| {
+	    match matcher.fuzzy_match(s, &self.search_input) {
+		Some(_) => Some(s.clone()),
+		None => None,
+	    }
+	}).collect();
+	self.filtered_edges = self.graph.edges.iter().flat_map(|Edge {from, val, to}| {
+	    if let Some(_) = matcher.fuzzy_match(from, &self.search_input) {
+		Some([from.clone(), match val {
+		    EdgeVal::Node(s) => s.clone(),
+		    _ => "".to_string(),
+		}, to.clone()])
+	    } else if let Some(_) = matcher.fuzzy_match(to, &self.search_input) {
+		Some([from.clone(), match val {
+		    EdgeVal::Node(s) => s.clone(),
+		    _ => "".to_string(),
+		}, to.clone()])
+	    } else if let EdgeVal::Node(s) = val {
+		if let Some(_) = matcher.fuzzy_match(s, &self.search_input) {
+		    Some([from.clone(), s.clone(), to.clone()])
+		} else {
+		    None
+		}
+	    } else {
+		None
+	    }
+	}).collect();
+    }
 }
 
 impl Sandbox for Aweb {
     type Message = Message;
     fn new() -> Self {
 	let mut graph = Graph::new();
-	graph.add_node("rex", NodeVal::Empty);
-	graph.add_node("king", NodeVal::Empty);
-	graph.add_node("Latin", NodeVal::Empty);
-	graph.add_node("amor", NodeVal::Empty);
-	graph.add_node("amoris", NodeVal::Empty);
-	graph.add_node("invenio", NodeVal::Empty);
-	graph.add_node("invenire", NodeVal::Empty);
-	graph.add_node("inveni", NodeVal::Empty);
-	graph.add_node("inventus", NodeVal::Empty);
-	graph.add_node("genitive singular", NodeVal::Empty);
-	graph.add_node("first declension", NodeVal::Empty);
-	graph.add_node("infinitive", NodeVal::Empty);
-	graph.add_node("third principle part", NodeVal::Empty);
-	graph.add_node("perfect passive participle", NodeVal::Empty);
-	graph.add_node("declension", NodeVal::Empty);
-	graph.add_edge(Edge::new("king", EdgeVal::Node("Latin".to_string()), "rex"));
-	graph.add_edge(Edge::new("amor", EdgeVal::Node("genitive singular".to_string()), "amoris"));
-	graph.add_edge(Edge::new("amor", EdgeVal::Node("declension".to_string()), "first declension"));
-	graph.add_edge(Edge::new("invenio", EdgeVal::Node("infinitive".to_string()), "invenire"));
-	graph.add_edge(Edge::new("invenio", EdgeVal::Node("third principle part".to_string()), "inveni"));
-	graph.add_edge(Edge::new("invenio", EdgeVal::Node("perfect passive participle".to_string()), "inventus"));
+
+	if let Some(project_dirs) = ProjectDirs::from("com", "semi", "aweb") {
+	    if let Ok(mut file) = File::open(project_dirs.data_dir().join(Path::new("graph.ron"))) {
+		let mut contents = String::new();
+		file.read_to_string(&mut contents).unwrap();
+		graph = ron::from_str(&contents).unwrap();
+	    }
+	}
 
 	graph.sort();
 
-	Self {
+	let mut aweb = Self {
 	    graph,
-	    node_input: "".to_string()
-	}
+	    filtered_nodes: vec![],
+	    filtered_edges: vec![],
+	    node_input: "".to_string(),
+	    search_input: "".to_string()
+	};
+	aweb.filter();
+	aweb
     }
 
     fn title(&self) -> String {
@@ -138,6 +166,7 @@ impl Sandbox for Aweb {
 		self.graph.nodes.insert(self.node_input.clone(), Node::new(&self.node_input.clone(), NodeVal::Empty));
 		self.node_input.clear();
 		self.graph.sort();
+		self.filter();
 	    },
 	    Message::SaveButtonPressed => {
 		let pretty = PrettyConfig::new()
@@ -145,27 +174,28 @@ impl Sandbox for Aweb {
 		let s = to_string_pretty(&self.graph, pretty).expect("Serialization failed");
 
 		if let Some(project_dirs) = ProjectDirs::from("com", "semi", "aweb") {
-		    fs::create_dir_all(project_dirs.data_dir());
+		    fs::create_dir_all(project_dirs.data_dir()).unwrap();
 		    let path = project_dirs.data_dir().join(Path::new("graph.ron"));
 		    let mut output = File::create(path).unwrap();
 		    write!(output, "{}", s).unwrap();
 		}
-	    }
+	    },
+	    Message::SearchInputChanged(s) => {
+		self.search_input = s.clone();
+		self.filter();
+	    },
 	}
     }
 
     fn view(&self) -> Element<Message> {
-	let edge_list = column(self.graph.edges.iter().map(|edge| Element::from(
-	    row![container(text(self.graph.nodes.get(edge.from.as_str()).unwrap().name.as_str())).width(Length::Fill),
-		 container(text(match &edge.val {
-		     EdgeVal::Empty => "",
-		     EdgeVal::Node(n) => self.graph.nodes.get(n.as_str()).unwrap().name.as_str()
-		 })).width(Length::Fill),
-		 container(text(self.graph.nodes.get(edge.to.as_str()).unwrap().name.as_str())).width(Length::Fill)]
+	let edge_list = column(self.filtered_edges.iter().map(|[from, val, to]| Element::from(
+	    row![container(text(from)).width(Length::Fill),
+		 container(text(val)).width(Length::Fill),
+		 container(text(to)).width(Length::Fill)]
 	)).collect())
 	    .spacing(4);
 
-	let node_list = column(self.graph.nodes.values().map(|node| Element::from(text(node.name.as_str()))).collect())
+	let node_list = column(self.filtered_nodes.iter().map(|s| Element::from(text(s))).collect())
 	    .spacing(4);
 	
 	container(column!(
@@ -176,8 +206,9 @@ impl Sandbox for Aweb {
 		 container(column![text("Nodes").size(40),
 				   scrollable(node_list).height(Length::Fill),
 				   text_input("add a node", &self.node_input, Message::NodeInputChanged)
-				   .on_submit(Message::NodeInputSubmit)]).width(Length::Fill)
-		 .width(Length::FillPortion(1))]))
+				   .on_submit(Message::NodeInputSubmit),
+		 ]).width(Length::Fill)].height(Length::FillPortion(5)),
+	    container(text_input("search", &self.search_input, Message::SearchInputChanged)).width(Length::Fill)))
 	    .width(Length::Fill)
 	    .height(Length::Fill)
 	    .padding(12)
@@ -190,4 +221,5 @@ pub enum Message {
     NodeInputChanged(String),
     NodeInputSubmit,
     SaveButtonPressed,
+    SearchInputChanged(String),
 }
